4a77e4be68826cc9c73f82a008f4b869bf62a202
Robson Tenório <rrtenorio@gmail.com>
2023-07-31 20:38:05 +0000

    WIP

--- resources/views/pages/docs/components/icon.blade.php
+++ resources/views/pages/docs/components/icon.blade.php
@@ -8,17 +8,17 @@ All icons are powered by [Blade Hero Icons](https://blade-ui-kit.com/blade-icons
 
 </x-markdown>
 
-<div class="grid gap-5 p-10 bg-gray-100 ">
-    <x-icon name="o-home" />
-    <x-icon name="s-home" class="w-7 h-7 text-purple-500" />
-    <x-icon name="s-home" class="w-10 h-10 bg-orange-500 p-2 text-white rounded-full" />
+<div class="grid gap-5 p-10 rounded-md bg-gray-100 ">
+    <x-icon name="o-envelope" />
+    <x-icon name="s-home" class="w-7 h-7 text-green-500" />
+    <x-icon name="o-home" class="w-10 h-10 bg-orange-500 p-2 text-white rounded-full" />
 </div>
 
 <pre>
 <x-torchlight-code language='html'>
-    <X-icon name="o-home" />
-    <X-icon name="s-home" class="w-7 h-7 text-purple-500" />
-    <X-icon name="s-home" class="w-10 h-10 bg-orange-500 p-2 text-white rounded-full" />
+    <X-icon name="o-envelope" />
+    <X-icon name="s-home" class="w-7 h-7 text-green-500" />
+    <X-icon name="o-home" class="w-10 h-10 bg-orange-500 p-2 text-white rounded-full" />
 </x-torchlight-code>
 </pre>
 
