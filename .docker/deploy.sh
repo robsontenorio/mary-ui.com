3d52037f492feb6f731ef489dc14a62e214a1365
Robson Tenório <rrtenorio@gmail.com>
2023-08-01 19:06:08 +0000

    WIP

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -5,13 +5,11 @@ echo '------ Starting deploy tasks  ------'
 cp .env.example .env
 composer install --prefer-dist --no-interaction --no-progress --ansi
 
+yarn install 
 yarn build
 
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
-
-# TODO: remove "fresh"
-php artisan migrate:fresh --force --seed
 
 echo '------ Deploy completed ------'
