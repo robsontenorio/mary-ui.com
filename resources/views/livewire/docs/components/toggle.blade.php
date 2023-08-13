b0d220f995829e4c6b94136636c6f51d805b2b62
Robson Tenório <rrtenorio@gmail.com>
2023-08-13 19:43:52 +0000

    WIP

--- resources/views/livewire/docs/components/toggle.blade.php
+++ resources/views/livewire/docs/components/toggle.blade.php
@@ -7,6 +7,8 @@ new class extends Component
     public bool $item1 = true;
 
     public bool $item2 = false;
+
+    public bool $item3 = false;
 }
 
 ?>
@@ -22,10 +24,13 @@ new class extends Component
     <x-toggle label="Left" wire:model="item1" />
     <hr class="my-5" />
 
-    <x-toggle label="Right" wire:model="item2" class="toggle-warning" right />
+    <x-toggle label="Right" wire:model="item2" right />
     <hr class="my-5" />    
 
-    <x-button label="Switch with $wire" @click="$wire.item2 = !$wire.item2"  class="btn-outline"/>
+    <x-toggle label="Right Tight" wire:model="item3" class="toggle-warning" right tight />
+    <hr class="my-5" />    
+
+    <x-button label="Switch with $wire" @click="$wire.item3 = !$wire.item3"  class="btn-outline"/>
     @endverbatim
 </div>
 </x-code>
