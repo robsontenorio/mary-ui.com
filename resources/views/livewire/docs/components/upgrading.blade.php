26581a903ae4aabfb5509d300ca7682eacc0c5bc
Robson Tenório <rrtenorio@gmail.com>
2023-10-25 12:29:48 -0300

    WIP

--- resources/views/livewire/docs/components/upgrading.blade.php
+++ resources/views/livewire/docs/components/upgrading.blade.php
@@ -10,17 +10,20 @@ new #[Title('Upgrading')] class extends Component {
     <x-anchor title="Upgrading" />
 
     <p>
-        Nothing special here, just a reminder:
+        You should keep an eye on Mary's <a href="https://github.com/robsontenorio/mary/releases">releases pages</a> to stay updated.
     </p>
 
-    <ul>
-        <li>
-            You should keep an eye on Mary's <a href="https://github.com/robsontenorio/mary/releases">releases pages</a> to stay updated.
-        </li>
-        <li>
-            As Mary uses <strong>daisyUI</strong> and <strong>Tailwind</strong> you should consider as well upgrade regularly their NPM packages.
-        </li>
-    </ul>
+    <x-code no-render language="bash">
+        composer require robsontenorio/mary
+    </x-code>
+
+    <p>
+        As Mary uses <strong>daisyUI</strong> and <strong>Tailwind</strong> you should consider as well upgrade regularly their NPM packages and dependencies.
+    </p>
+
+    <x-code no-render language="bash">
+        yarn add --D daisyui tailwindcss postcss autoprefixer
+    </x-code>
 
     <x-anchor title="Recent releases" size="text-2xl" class="mt-10 mb-5" />
 
