7b4214b8b1fc002af71d8eab0fbded8be2a9250b
Robson Tenório <rrtenorio@gmail.com>
2023-11-11 20:34:44 -0300

    WIP

--- resources/views/components/layouts/landing.blade.php
+++ resources/views/components/layouts/landing.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title ?? config('app.name') }}</title>
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/favicon.ico') }}">
