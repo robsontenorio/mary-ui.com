e16c7dad8f8bd3d008f895c24c079ae49ee6e02d
Robson Tenório <rrtenorio@gmail.com>
2023-11-14 14:22:16 -0300

    WIP

--- resources/views/livewire/docs/components/menu.blade.php
+++ resources/views/livewire/docs/components/menu.blade.php
@@ -19,33 +19,95 @@ class extends Component {
         sidebar slot.
     </p>
 
+    <x-anchor title="Basic" size="text-2xl" class="mt-10 mb-5" />
+
     <x-code class="grid gap-5 justify-center">
         @verbatim('docs')
             {{-- Auto activate menu item with `activate-by-route`--}}
-            <x-menu activate-by-route active-bg-color="bg-blue-50" class="border border-dashed">
+            <x-menu class="border border-dashed">
+                <x-menu-item title="Home" icon="o-envelope" />
+                <x-menu-item title="Messages" icon="o-paper-airplane" badge="78+" />
+                <x-menu-item title="Hello" icon="o-sparkles" badge="new" badge-classes="!badge-warning" />
 
-                <x-menu-item title="Messages" icon="o-envelope" badge="78+" />
-                <x-menu-item title="Navigate to Alert docs" icon="o-arrow-right" link="/docs/components/alert" />
+                <x-menu-item title="Internal link" icon="o-arrow-down" link="/docs/components/alert" />
 
+                {{-- Notice external --}}
+                <x-menu-item title="External link" icon="o-arrow-uturn-right" link="https://google.com" external />
+            </x-menu>
+        @endverbatim
+    </x-code>
+
+    <x-anchor title="Sections and Sub-menus" size="text-2xl" class="mt-10 mb-5" />
+
+    <x-code class="grid gap-5 justify-center">
+        @verbatim('docs')
+            <x-menu class="border border-dashed">
+                <x-menu-item title="Hello" />
+                <x-menu-item title="There" />
+
+                {{-- Simple separator --}}
                 <x-menu-separator />
 
+                {{-- Submenu --}}
                 <x-menu-sub title="Settings" icon="o-cog-6-tooth">
                     <x-menu-item title="Wifi" icon="o-wifi" />
                     <x-menu-item title="Archives" icon="o-archive-box" />
                 </x-menu-sub>
 
+                {{-- Separator with title and icon --}}
                 <x-menu-separator title="Magic" icon="o-sparkles" />
-                <x-menu-item title="Hello" badge="7" badge-classes="!badge-warning !text-red-500" />
 
-                {{-- When route matches `link` property it activates menu --}}
-                <x-menu-item title="Active state" link="/docs/components/menu" />
+            </x-menu>
 
-                <x-menu-separator title="Tricks" />
-                <x-menu-item title="Hi" />
+        @endverbatim
+    </x-code>
+
+    <x-anchor title="Active state" size="text-2xl" class="mt-10 mb-5" />
+
+    <p>
+        You can manually define the active menu item by placing <code>active</code> attribute and choose a custom active color with <code>active-bg-color</code> attribute.
+    </p>
+
+    <x-code class="grid gap-5 justify-center">
+        @verbatim('docs')
+            <x-menu active-bg-color="bg-blue-50" class="border border-dashed">
+                {{-- Notice `active` --}}
+                <x-menu-item title="Hi" active />
                 <x-menu-item title="Some style" class="text-purple-500 font-bold" />
-
             </x-menu>
         @endverbatim
     </x-code>
 
+    <br>
+
+    <p>
+        You can automatically activate a menu item when current route matches <code>link</code> and its nested route variations by using the <code>activate-by-route</code>
+        attribute.
+    </p>
+    <p>
+        For example, <code>link="/users"</code> will activate same menu item for:
+    </p>
+    <ul>
+        <li>/users</li>
+        <li>/users/23</li>
+        <li>/users/23/edit</li>
+        <li>/users/23/create</li>
+        <li>/users/?q=mary</li>
+        <li>...</li>
+    </ul>
+
+    <br>
+
+    <x-code class="grid gap-5 justify-center">
+        @verbatim('docs')
+            {{-- Notice `activate-by-route` --}}
+            <x-menu activate-by-route class="border border-dashed">
+                {{-- It is active because you are right now browsing this url --}}
+                <x-menu-item title="Home" link="/docs/components/menu" />
+
+                <x-menu-item title="Party" />
+            </x-menu>
+
+        @endverbatim
+    </x-code>
 </div>
