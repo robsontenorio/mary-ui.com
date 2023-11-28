8bbee6b23568eed1a5af27b55f82d0c0ef8723e1
Robson Tenório <rrtenorio@gmail.com>
2023-11-28 17:31:57 -0300

    WIP

--- resources/views/livewire/docs/components/modal.blade.php
+++ resources/views/livewire/docs/components/modal.blade.php
@@ -21,7 +21,7 @@ class extends Component {
     <x-anchor title="Native HTML" size="text-2xl" class="mt-10 mb-5" />
 
     <p>
-        Note the following examples that <code>onclick</code> , <code>.showModal()</code> and <code>.close()</code> are native HTML stuff, not Mary/Livewire/Alpine.
+        Notice the following examples that <code>onclick</code> , <code>.showModal()</code> and <code>.close()</code> are native HTML stuff, not Mary/Livewire/Alpine.
     </p>
 
     <br>
