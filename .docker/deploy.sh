96996811cbfb8c2a0ac4504258e0bf04cd30efb3
Robson Tenório <rrtenorio@gmail.com>
2024-01-19 23:28:32 -0300

    WIP

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -11,6 +11,7 @@ yarn build
 touch database/database.sqlite
 php artisan migrate:fresh --seed --force
 
+php artisan storage:link
 php artisan config:cache
 php artisan view:cache
 php artisan route:cache
