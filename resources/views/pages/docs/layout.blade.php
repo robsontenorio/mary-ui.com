285397e5538730f7aba9d7a59c9f2db7c1f2da37
Robson Tenório <rrtenorio@gmail.com>
2024-03-07 14:06:44 -0300

    WIP

--- resources/views/pages/docs/layout.blade.php
+++ resources/views/pages/docs/layout.blade.php
@@ -19,13 +19,15 @@ new #[Title('Layout')] class extends Component {
         <code>views/components/layouts/app.blade.php</code>.
     </p>
 
+    <x-alert icon="o-light-bulb">
+        If you have used the <strong>automatic installer</strong> in a brand-new project without Jetstream/Breeze
+        you already have the "Only sidebar" layout in place.
+    </x-alert>
+
     <x-anchor title="Only sidebar" size="text-2xl" class="mt-10 mb-5" />
 
     <p>
         This layout has only a collapsible Sidebar. It fits nice for an "enterprise layout", which needs more vertical spacing.
-    </p>
-    <p>
-        If you have used the <strong>automatic installer</strong> you already have this layout in place.
     </p>
 
     <x-code no-render>
@@ -43,9 +45,8 @@ new #[Title('Layout')] class extends Component {
 
                 {{-- NAVBAR mobile only --}}
                 <x-nav sticky class="lg:hidden">
-                    <x-slot:brand class="flex gap-2 items-center">
-                        <x-icon name="o-square-3-stack-3d" class="text-primary" />
-                        <div>App</div>
+                    <x-slot:brand>
+                        <div class="ml-5 pt-5">App</div>
                     </x-slot:brand>
                     <x-slot:actions>
                         <label for="main-drawer" class="lg:hidden mr-3">
@@ -60,21 +61,22 @@ new #[Title('Layout')] class extends Component {
                     <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
 
                         {{-- BRAND --}}
-                        <div class="p-6 pt-3 flex gap-3 items-center h-20">
-                            <x-icon name="o-square-3-stack-3d" class="text-primary" />
-                            <div class="hidden-when-collapsed">App</div>
-                        </div>
+                        <div class="ml-5 pt-5">App</div>
 
                         {{-- MENU --}}
                         <x-menu activate-by-route>
 
                             {{-- User --}}
                             @if($user = auth()->user())
-                                <x-list-item :item="$user" sub-value="username" no-separator no-hover class="!-mx-2 mt-2 mb-5 border-y border-y-sky-900">
+                                <x-menu-separator />
+
+                                <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
                                     <x-slot:actions>
-                                        <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" />
+                                        <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
                                     </x-slot:actions>
                                 </x-list-item>
+
+                                <x-menu-separator />
                             @endif
 
                             <x-menu-item title="Hello" icon="o-sparkles" link="/" />
@@ -91,19 +93,13 @@ new #[Title('Layout')] class extends Component {
                     </x-slot:content>
                 </x-main>
 
-                {{--  TOAST area --}}
+                {{-- Toast --}}
                 <x-toast />
             </body>
         @endverbatim
     </x-code>
 
-    <x-anchor title="All together" size="text-2xl" class="mt-10 mb-5" />
-
-    <ul>
-        <li>Navbar</li>
-        <li>Sidebar</li>
-        <li>Footer</li>
-    </ul>
+    <x-anchor title="With Navbar" size="text-2xl" class="mt-10 mb-5" />
 
     <x-code no-render>
         @verbatim('docs')
@@ -116,7 +112,7 @@ new #[Title('Layout')] class extends Component {
                 @vite(['resources/css/app.css', 'resources/js/app.js'])
             </head>
 
-            <body class="min-h-screen font-sans antialiased">
+            <body class="font-sans antialiased">
 
                 {{-- The navbar with `sticky` and `full-width` --}}
                 <x-nav sticky full-width>
@@ -133,8 +129,8 @@ new #[Title('Layout')] class extends Component {
 
                     {{-- Right side actions --}}
                     <x-slot:actions>
-                        <x-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" />
-                        <x-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" />
+                        <x-button label="Messages" icon="o-envelope" link="###" class="btn-ghost btn-sm" responsive />
+                        <x-button label="Notifications" icon="o-bell" link="###" class="btn-ghost btn-sm" responsive />
                     </x-slot:actions>
                 </x-nav>
 
@@ -143,12 +139,27 @@ new #[Title('Layout')] class extends Component {
 
                     {{-- This is a sidebar that works also as a drawer on small screens --}}
                     {{-- Notice the `main-drawer` reference here --}}
-                    <x-slot:sidebar drawer="main-drawer" class="bg-slate-200">
+                    <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-200">
+
+                        {{-- User --}}
+                        @if($user = auth()->user())
+                            <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="pt-2">
+                                <x-slot:actions>
+                                    <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
+                                </x-slot:actions>
+                            </x-list-item>
+
+                            <x-menu-separator />
+                        @endif
 
                         {{-- Activates the menu item when a route matches the `link` property --}}
                         <x-menu activate-by-route>
                             <x-menu-item title="Home" icon="o-home" link="###" />
                             <x-menu-item title="Messages" icon="o-envelope" link="###" />
+                            <x-menu-sub title="Settings" icon="o-cog-6-tooth">
+                                <x-menu-item title="Wifi" icon="o-wifi" link="####" />
+                                <x-menu-item title="Archives" icon="o-archive-box" link="####" />
+                            </x-menu-sub>
                         </x-menu>
                     </x-slot:sidebar>
 
@@ -156,15 +167,10 @@ new #[Title('Layout')] class extends Component {
                     <x-slot:content>
                         {{ $slot }}
                     </x-slot:content>
-
-                    {{-- Footer area --}}
-                    <x-slot:footer>
-                        <hr />
-                        <div class="p-6">
-                            Footer
-                        </div>
-                    </x-slot:footer>
                 </x-main>
+
+                {{--  TOAST area --}}
+                <x-toast />
             </body>
         @endverbatim
     </x-code>
