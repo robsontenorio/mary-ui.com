23c6c53b540651461196d59317a18a195998632e
Robson Tenório <rrtenorio@gmail.com>
2024-03-27 11:06:48 -0300

    WIP

--- resources/views/livewire/docs/components/form.blade.php
+++ resources/views/livewire/docs/components/form.blade.php
@@ -4,6 +4,7 @@ use Livewire\Attributes\Layout;
 use Livewire\Attributes\Rule;
 use Livewire\Attributes\Title;
 use Livewire\Volt\Component;
+use Illuminate\Support\Facades\Validator;
 
 new
 #[Title('Form')]
@@ -13,17 +14,29 @@ class extends Component {
 
     public ?float $amount = null;
 
+    public ?string $address = null;
+
+    public ?int $number = null;
+
     public ?string $email = null;
 
     public ?int $age = null;
 
-    public $state = [
+    public ?string $magicWord1 = null;
+
+    public ?string $magicWord2 = null;
+
+    public ?int $salary = null;
+
+    public ?string $full_name = null;
+
+    public array $state = [
         'name' => null,
         'amount' => null,
         'multiple' => null
     ];
 
-    public function save()
+    public function save(): void
     {
         sleep(1);
 
@@ -33,7 +46,43 @@ class extends Component {
         ]);
     }
 
-    public function save2()
+    public function save2(): void
+    {
+        sleep(1);
+
+        $this->validate([
+            'address' => 'required|min:20',
+            'number' => 'required|integer'
+        ]);
+    }
+
+    public function save3(): void
+    {
+        sleep(1);
+
+        $this->validate([
+            'magicWord1' => 'starts_with:Hello|ends_with:world',
+            'magicWord2' => 'starts_with:Hello|ends_with:world',
+        ]);
+    }
+
+    public function save4(): void
+    {
+        sleep(1);
+
+        $this->addError('total_salary', 'This is a custom error message for total salary field.');
+    }
+
+    public function save5(): void
+    {
+        sleep(1);
+
+        $this->validate([
+            'full_name' => 'required|min:20',
+        ]);
+    }
+
+    public function save6(): void
     {
         sleep(1);
 
@@ -41,17 +90,6 @@ class extends Component {
             'email' => 'required|email',
             'age' => 'required|integer'
         ]);
-    }
-
-    public function save3()
-    {
-        sleep(1);
-
-        \Illuminate\Support\Facades\Validator::make($this->state, [
-            'title' => 'required|min:20',
-            'cost' => 'required|decimal:0,2',
-            'multiple' => 'required|starts_with:Hello|ends_with:world',
-        ])->validate();
     }
 }
 
@@ -93,50 +131,39 @@ class extends Component {
         @endverbatim
     </x-code>
 
-    <x-anchor title="Validation Error Attributes" size="text-2xl" class="mt-10 mb-5" />
+    <x-anchor title="Omit errors" size="text-2xl" class="mt-10 mb-5" />
     <p>
-        All input components in this library support various attributes to customize validation error behavior and
-        appearance. By default, components search for validation errors in the default error bag corresponding to the
-        <code>wire:model</code> property name.
+        If for some reason you want to omit the error message, you can use the <code>omit-error</code> attribute.
     </p>
+
+    <x-code>
+        @verbatim('docs')
+            <x-form wire:submit="save2">
+                <x-input label="Address" wire:model="address" />
+
+                {{-- Notice `omit-error` --}}
+                <x-input label="Number" wire:model="number" omit-error hint="This is required, but we suppress the error message" />
+
+                <x-slot:actions>
+                    <x-button label="Cancel" />
+                    <x-button label="Click me!" class="btn-primary" type="submit" spinner="save2" />
+                </x-slot:actions>
+            </x-form>
+        @endverbatim
+    </x-code>
+
+    <x-anchor title="First message only" size="text-2xl" class="mt-10 mb-5" />
     <p>
-        To tailor the validation error handling to your needs, the following attributes are available:
+        If you have multiple validation messages for the same filed and want to show only the first error message, you can use the <code>first-error-only</code> attribute.
     </p>
-    <ul class="list-disc pl-5 mb-4">
-        <li><strong>error-field</strong>: Specifies the name of the error field to be referenced. Supports both specific
-            attribute values and wildcard patterns (e.g.,<code>state.val</code> or <code>val.*</code>).
-        </li>
-        <li><strong>omit-error</strong>: When set to <code>true</code>, the component's border will be highlighted
-            without displaying the error text.
-        </li>
-        <li><strong>first-error-only</strong>: Setting this to <code>true</code> ensures that only the first encountered
-            error is displayed.
-        </li>
-        <li><strong>error-class</strong>: Provides an option to override the CSS classes for the error message div.</li>
-    </ul>
 
-    <x-anchor title="Default Values" size="text-2xl" class="mt-10 mb-5" />
-    <p>The following are the default values for each attribute:</p>
-    <ul class="list-disc pl-5 mb-4">
-        <li><code>error-field</code> defaults to <code>$modelName()</code>.</li>
-        <li><code>omit-error</code> is <code>false</code> by default, meaning error texts are shown.</li>
-        <li><code>first-error-only</code> is set to <code>false</code>, allowing all relevant errors to be displayed.
-        </li>
-        <li>The default <code>error-class</code> is <code>'text-red-500 label-text-alt p-1'</code>.</li>
-    </ul>
-
-    <x-code x-classes="text-blue-500">
+    <x-code>
         @verbatim('docs')
             <x-form wire:submit="save3">
-                <x-input label="Title" wire:model="state.title" error-field="title" />
-                <x-input label="Cost" wire:model="state.cost" prefix="USD" money hint="It submits an unmasked value"
-                         error-field="cost" :omit-error="true" />
+                <x-input label="Magic word 1" wire:model="magicWord1" />
 
-                <x-input label="Multiple Messages" wire:model="state.multiple" error-field="multiple" />
-                <x-input label="One Message" wire:model="state.multiple" error-field="multiple" :first-error-only="true" />
-
-                <x-input label="Blue text" wire:model="state.multiple" error-field="multiple"
-                         error-class="text-blue-500 label-text-alt p-1" />
+                {{-- Notice `first-error-only`--}}
+                <x-input label="Magic word 2" wire:model="magicWord2" first-error-only />
 
                 <x-slot:actions>
                     <x-button label="Cancel" />
@@ -146,31 +173,85 @@ class extends Component {
         @endverbatim
     </x-code>
 
-    <x-anchor title="Error bag" size="text-2xl" class="mt-10 mb-5" />
-    <p>
-        As you can see above, all validation errors are automatically displayed for each input.
-        Additionally, you can display <strong>entire error bag</strong> or <strong>omit</strong> error handling for some
-        inputs.
-    </p>
+    {{--@formatter:off--}}
+    <x-code no-render language="php">
+        @verbatim('docs')
+            $this->validate([
+                'magicWord1' => 'starts_with:Hello|ends_with:world',
+                'magicWord2' => 'starts_with:Hello|ends_with:world',
+            ]);
+        @endverbatim
+    </x-code>
+    {{--@formatter:on--}}
 
+    <x-anchor title="Custom error field" size="text-2xl" class="mt-10 mb-5" />
     <p>
-        Currently, it <strong>does not work</strong> with multiple forms on same screen.
+        If you want to display validation errors for a custom error field, you can use the <code>error-field</code> attribute.
     </p>
 
     <x-code>
         @verbatim('docs')
-            <x-form wire:submit="save2">
+            <x-form wire:submit="save4">
+                {{-- Notice `error_field` --}}
+                <x-input label="Custom error field" wire:model="salary" error-field="total_salary" />
+
+                <x-slot:actions>
+                    <x-button label="Cancel" />
+                    <x-button label="Click me!" class="btn-primary" type="submit" spinner="save4" />
+                </x-slot:actions>
+            </x-form>
+        @endverbatim
+    </x-code>
+
+    {{--@formatter:off--}}
+    <x-code no-render language="php">
+        @verbatim('docs')
+            $this->addError('total_salary', 'This is a custom error message for total salary field.');
+        @endverbatim
+    </x-code>
+    {{--@formatter:on--}}
+
+    <x-anchor title="Custom error style" size="text-2xl" class="mt-10 mb-5" />
+    <p>
+        You can customize the error message style by using the <code>error-class</code> attribute.
+    </p>
+
+    <x-code>
+        @verbatim('docs')
+            <x-form wire:submit="save5">
+                {{--  Custom CSS class. Remeber to configure Tailwind safelist --}}
+                <x-input label="Full name" wire:model="full_name" error-class="bg-blue-500 p-1" />
+
+                <x-slot:actions>
+                    <x-button label="Cancel" />
+                    <x-button label="Click me!" class="btn-primary" type="submit" spinner="save5" />
+                </x-slot:actions>
+            </x-form>
+        @endverbatim
+    </x-code>
+
+    <x-anchor title="Full error bag" size="text-2xl" class="mt-10 mb-5" />
+    <p>
+        As you can see above, all validation errors are automatically displayed for each input.
+        Additionally, you can display <strong>entire error bag</strong> with <code>x-errors</code> component.
+    </p>
+
+    <x-alert icon="o-light-bulb" class="my-5">
+        Currently, it <strong>does not work</strong> with multiple forms on same screen.
+    </x-alert>
+
+    <x-code>
+        @verbatim('docs')
+            <x-form wire:submit="save6">
                 {{-- Full error bag --}}
                 {{-- All attributes are optional, remove it and give a try--}}
                 <x-errors title="Oops!" description="Please, fix them." icon="o-face-frown" />
 
                 <x-input label="Age" wire:model="age" />
-
-                {{-- Notice `omit-error`--}}
-                <x-input label="E-mail" wire:model="email" hint="It will omit error here" omit-error />
+                <x-input label="E-mail" wire:model="email" />
 
                 <x-slot:actions>
-                    <x-button label="Click me!" class="btn-primary" type="submit" spinner="save2" />
+                    <x-button label="Click me!" class="btn-primary" type="submit" spinner="save6" />
                 </x-slot:actions>
             </x-form>
         @endverbatim
