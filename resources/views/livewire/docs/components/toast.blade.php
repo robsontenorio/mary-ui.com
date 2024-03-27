56a2c7a62cbdb679e05972d9404c7c3a7781959b
Jed Jones <jedjones-uk@users.noreply.github.com>
2024-03-27 16:45:44 +0000

    Include details about ToastException in the toast docs page (#31)
    Co-authored-by: Robson Tenório <rrtenorio@gmail.com>

--- resources/views/livewire/docs/components/toast.blade.php
+++ resources/views/livewire/docs/components/toast.blade.php
@@ -201,18 +201,89 @@ class extends Component {
         @verbatim('docs')
             public function save4()
             {
-                // Your stuff here ...
+            // Your stuff here ...
 
-                // Toast
-                $this->warning(
-                    'Wishlist <u>updated</u>',
-                    'You will <strong>love it :)</strong>',
-                    position: 'bottom-end',
-                    icon: 'o-heart',
-                    css: 'bg-pink-500 text-base-100'
-                );
+            // Toast
+            $this->warning(
+            'Wishlist <u>updated</u>',
+            'You will <strong>love it :)</strong>',
+            position: 'bottom-end',
+            icon: 'o-heart',
+            css: 'bg-pink-500 text-base-100'
+            );
             }
         @endverbatim
     </x-code>
     {{--@formatter:on--}}
+
+    <x-anchor title="Using an Exception" size="text-2xl" class="mt-10 mb-5" />
+
+    <p>
+        If you are trying to trigger a toast from outside a Livewire context, you can use the <code>ToastException</code> to do so.
+        This class has a number of shortcut functions to make it easier to use, and it's possible to overwrite all defaults.
+    </p>
+
+    {{--@formatter:off--}}
+    <x-code no-render language="php">
+        @verbatim('docs')
+        use Mary\Exceptions\ToastException;
+
+        public function notALivewireMethod()
+        {
+            throw ToastException::error('Your operation could not complete','Uh Oh!');
+
+            // Shortcuts
+            throw ToastException::success(...);
+            throw ToastException::error(...);
+            throw ToastException::warning(...);
+            throw ToastException::info(...);
+        }
+        @endverbatim
+    </x-code>
+    {{--@formatter:on--}}
+
+    <p>
+        The final options array is useful when you need to tweak the default configuration in the place you've used it.
+        The key and values of the array match the property names in the <code>Toast</code> trait method.
+    </p>
+
+    {{--@formatter:off--}}
+    <x-code no-render language="php">
+        @verbatim('docs')
+        use Mary\Exceptions\ToastException;
+
+        public function notALivewireMethod()
+        {
+            $options = [
+                'icon'=>'o-exclamation-triangle'
+                'position' => 'toast-bottom toast-start',
+                'css' => 'alert-warning text-lg',
+                'timeout' => 5000,
+            ];
+            throw ToastException::info('Description Text or message', 'Title', $options);
+        }
+        @endverbatim
+    </x-code>
+    {{--@formatter:on--}}
+
+    <p>
+        The livewire request hook <code>fail</code> method is used to handle the client side rendering of the toasts. If you already have hooks set up, the hook to render the toast will be called second.
+        If you have a toast call in your existing hook, it will be de-bounced so only one call is used.
+        The livewire fail hook is given a <code>preventDefault()</code> function to call if you wish to stop the event bubbling up, this behaviour is respected by the second hook configured by toast.
+        If you want to disable this call, you can chain the <code>permitDefault()</code> method on your exception.
+    </p>
+
+    {{--@formatter:off--}}
+    <x-code no-render language="php">
+        @verbatim('docs')
+        use Mary\Exceptions\ToastException;
+
+        public function notALivewireMethod()
+        {
+            throw ToastException::info('Do not prevent default on client side')->permitDefault();
+        }
+        @endverbatim
+    </x-code>
+    {{--@formatter:on--}}
+
 </div>
