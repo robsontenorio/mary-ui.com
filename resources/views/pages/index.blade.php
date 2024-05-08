d38550b94ac48d072e454710282151513da6742f
Robson Tenório <rrtenorio@gmail.com>
2024-05-08 13:48:06 -0300

    WIP

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -133,11 +133,11 @@ new #[Layout('components.layouts.landing')] class extends Component {
             <div>
                 @php
                     $images = [
-                        'https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg',
-                        'https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg',
-                        'https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg',
-                        'https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg',
-                        'https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg',
+                        '/photos/photo-1559703248-dcaaec9fab78.jpg',
+                        '/photos/photo-1572635148818-ef6fd45eb394.jpg',
+                        '/photos/photo-1565098772267-60af42b81ef2.jpg',
+                        '/photos/photo-1494253109108-2e30c049369b.jpg',
+                        '/photos/photo-1550258987-190a2d41a8ba.jpg',
                     ]
                 @endphp
 
