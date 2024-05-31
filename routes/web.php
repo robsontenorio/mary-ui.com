300a63e3493615d464d87e9f66ba58c88116a7bc
Robson Tenório <rrtenorio@gmail.com>
2024-05-31 13:08:40 -0300

    WIP

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,7 @@ Volt::route('/docs/components/header', 'docs.components.header');
 Volt::route('/docs/components/icon', 'docs.components.icon');
 Volt::route('/docs/components/kbd', 'docs.components.kbd');
 Volt::route('/docs/components/pin', 'docs.components.pin');
+Volt::route('/docs/components/popover', 'docs.components.popover');
 Volt::route('/docs/components/progress', 'docs.components.progress');
 Volt::route('/docs/components/spotlight', 'docs.components.spotlight');
 Volt::route('/docs/components/statistic', 'docs.components.statistic');
