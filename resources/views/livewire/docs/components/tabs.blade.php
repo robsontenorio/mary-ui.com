28bdf6ce79a86ff5dcbb52764d199013c1676090
Robson Tenório <rrtenorio@gmail.com>
2024-06-16 18:42:14 -0300

    WIP

--- resources/views/livewire/docs/components/tabs.blade.php
+++ resources/views/livewire/docs/components/tabs.blade.php
@@ -66,27 +66,25 @@ class extends Component {
 
     <x-anchor title="Customisation" size="text-2xl" class="mt-10 mb-5" />
 
-    <p>
-       Add custom classes to the x-tabs component
-    </p>
+    <x-alert icon="o-light-bulb" class="markdown mb-10">
+        Remember to add these custom classes on Tailwind <strong>safelist</strong>.
+    </x-alert>
+
     <x-code>
         @verbatim('docs')
-            <x-tabs label-class="font-semibold" label-div-class="flex overflow-x-auto w-full" active-class='bg-primary'
-                   wire:model="selectedTab"
-                   class="!border-none w-full">
-                <x-tab id="all-tab" name="all-tab" label="All" class="!border-none">
-                    <div class="w-full flex flex-col items-start justify-start gap-4 !border-none">
-                        <div>All</div>
-                    </div>
+            <x-tabs
+                wire:model="selectedTab"
+                active-class="bg-primary rounded text-white"
+                label-class="font-semibold"
+                label-div-class="bg-primary/5 p-2 rounded"
+            >
+                <x-tab name="users-tab" label="Users">
+                    <div>All</div>
                 </x-tab>
-                <x-tab name="graded-tab" label="Graded">
+                <x-tab name="tricks-tab" label="Tricks">
                     <div>Tricks</div>
                 </x-tab>
-                <x-tab name="ungraded-tab">
-                    <x-slot:label>
-                        Ungraded
-                        <x-badge value="3" class="badge-primary"/>
-                    </x-slot:label>
+                <x-tab name="musics-tab" label="Musics">
                     <div>Musics</div>
                 </x-tab>
             </x-tabs>
