46c5f9fa50bf853eeb4d2a07419491ee19abccd1
Robson Tenório <rrtenorio@gmail.com>
2024-08-17 23:56:53 -0300

    WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,12 +12,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    
     protected $guarded = ['id'];
 
     /**
