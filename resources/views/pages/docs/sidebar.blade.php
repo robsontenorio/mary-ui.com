8f735ccb170f1142f0c5861fd3f5d6a9142ca378
Emanuel Martinez <emanuelmartinezw@hotmail.com>
2024-09-06 18:27:47 -0300

    fix: collapse-text attribute on sidebar

--- resources/views/pages/docs/sidebar.blade.php
+++ resources/views/pages/docs/sidebar.blade.php
@@ -26,7 +26,7 @@ class extends Component {
             <x-slot:sidebar
                 drawer="main-drawer"          {{-- Drawer ID trigger for mobile --}}
                 collapsible                   {{-- Make it collapsible --}}
-                collapse-text="Hide it"    {{-- Custom collapsible text --}}
+                collapse-text="Hide it"       {{-- Custom collapsible text --}}
                 class="bg-base-100"           {{-- Any Tailwind class--}}
                 right                         {{-- Move it to the right side --}}
                 right-mobile                  {{-- Move the drawer to the right side only for mobile devices --}}
