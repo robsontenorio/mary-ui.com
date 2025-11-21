2691bfd8d2c180ae0bf1f72b4cea3f56ae1336f1
Robson Tenório <rrtenorio@gmail.com>
2025-11-21 10:18:48 -0300

    WIP

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/zsh
 
-echo '------ Starting deploy tasks  ------'
-
 cp .env.example .env
 composer install --prefer-dist --no-interaction --no-progress --ansi
 
@@ -9,12 +7,11 @@ yarn install
 yarn build
 
 touch database/database.sqlite
+
+# DO NOT this in real world.
+# This refresh the database on every deploy
 php artisan migrate:fresh --seed --force
 
 php artisan storage:link
-php artisan config:cache
-php artisan view:cache
-php artisan route:cache
+php artisan optimize
 php artisan icons:cache
-
-echo '------ Deploy completed ------'
